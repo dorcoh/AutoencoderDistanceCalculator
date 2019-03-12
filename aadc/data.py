3dddfaceeb47ea0723104636f14a06b20211fe11
Dor <dor.coh@gmail.com>
2019-03-12 23:10:18 +0200

    add parameters to get dataloader

--- aadc/data.py
+++ aadc/data.py
@@ -3,18 +3,14 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 
 
-class Data:
-    def __init__(self, batch_size):
-        self.batch_size = batch_size
-        self.dataset = None
-        self.dataloader = None
+def get_datalodaer(batch_size, normalize=False, shuffle=False):
+    """gets pytorch mnist dataloader"""
+    transform = [transforms.ToTensor()]
+    if normalize:
+        transform.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+    img_transform = transforms.Compose(transform)
 
-    def get_datalodaer(self):
-        img_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+    dataset = MNIST('./data', transform=img_transform, download=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
-        self.dataset = MNIST('./data', transform=img_transform, download=True)
-        self.dataloader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
-        return self.dataloader
+    return dataloader
